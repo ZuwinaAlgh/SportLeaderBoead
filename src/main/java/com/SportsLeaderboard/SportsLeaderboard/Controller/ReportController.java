8ac82ef605eda895265f08fdde4242dba837f53e
ZuwinaAlgh <Z.alghafri@hotmail.com>
2023-05-16 16:51:13 +0400

    update task

--- src/main/java/com/SportsLeaderboard/SportsLeaderboard/Controller/ReportController.java
+++ src/main/java/com/SportsLeaderboard/SportsLeaderboard/Controller/ReportController.java
@@ -1,9 +1,22 @@
 package com.SportsLeaderboard.SportsLeaderboard.Controller;
 
+import com.SportsLeaderboard.SportsLeaderboard.Service.ReportService;
+import net.sf.jasperreports.engine.JRException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileNotFoundException;
 
 @RequestMapping(value="report")
 @RestController
 public class ReportController {
+
+    @Autowired
+    ReportService reportService;
+
+    @RequestMapping(value="generateLeaderBoardReports")
+    public String generateLeaderBoardReports() throws JRException, FileNotFoundException {
+        return reportService.generateLeaderBoardReports();
+    }
 }
--- src/main/java/com/SportsLeaderboard/SportsLeaderboard/DTO/LeaderBoardReports.java
+++ src/main/java/com/SportsLeaderboard/SportsLeaderboard/DTO/LeaderBoardReports.java
@@ -17,13 +17,14 @@ public class LeaderBoardReports {
     Integer win;
     Integer loss;
 
-    public void leaderBoardReports(String userName,String password, String teamName,String sport,Integer score,Integer win,Integer loss){
-      this.userName=userName;
-      this.password=password;
-      this.teamName=teamName;
-      this.sport=sport;
-      this.score=score;
-      this.win=win;
-      this.loss=loss;
+
+    public LeaderBoardReports(String userName, String password, String teamName, String sport, Integer score, Integer win, Integer loss) {
+        this.userName = userName;
+        this.password = password;
+        this.teamName = teamName;
+        this.sport = sport;
+        this.score = score;
+        this.win = win;
+        this.loss = loss;
     }
 }
